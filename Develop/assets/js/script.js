c80d5eec0afc324bbbbec1198fc3043bd65b347e
connorshea15 <connorshea15@gmail.com>
2020-06-26 18:33:52 -0700

    I am saving my task objects to local storage and retrieving them with a refreshed page

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,12 +1,39 @@
 // This is a javascript file 
+// This is the array to hold my task objects
+var tasks = [];
 
 // Display the current day at the top of the page
 $("#currentDay").append(moment().format('dddd, MMMM Do'));
 
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    for (i = 0; i < tasks.length; i++) {
+        console.log(tasks[i]);
+    }
+
+};
+
+
+// Function to save tasks to local storage
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
 // I need to create a function to save the shiz to local storage
 $(".row").on("click", ".saveBtn", function() {
     // Grab the text from the task input
-    newTask = $(this).prev().val().trim();
-    console.log(newTask);
+    newTask = $(this).prev();
+    taskId = newTask.attr("id");
+    
+    tasks.push({
+        text: newTask.val().trim(),
+        identifier: taskId
+    });
+
+    console.log(tasks);
+    saveTasks();
     
 });
+
+loadTasks();
