db5079828547c28c45d46634366a6790c5c050e2
connorshea15 <connorshea15@gmail.com>
2020-06-26 17:39:22 -0700

    Just doing stuff

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -3,3 +3,8 @@
 // Display the current day at the top of the page
 $("#currentDay").append(moment().format('dddd, MMMM Do'));
 
+// I need to create a function to save the shiz to local storage
+$(".saveBtn").on("click", "textarea", function() {
+    
+
+});
